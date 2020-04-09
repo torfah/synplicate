94f616c072bf36b2616d61ee0a2681f0ee9c9c02
Learn and Verify <lv@Learns-MacBook-Pro.local>
2020-04-08 17:40:18 -0700

    move sampler to bechnmark folder

--- examples/california_census_simplified/sampler.py
+++ examples/california_census_simplified/sampler.py
@@ -44,25 +44,25 @@ def sample(model,num_of_samples):
         feature1_value = random.uniform(-124.3,-114.3)
         print(feature1_name, ": ", feature1_value,end =" | ")
         feature1_value_norm = truncate((feature1_value - train_df_mean[feature1_name])/train_df_std[feature1_name],2)
-        print(feature1_name, "_norm: ", feature1_value_norm,end =" | ")
+        # print(feature1_name, "_norm: ", feature1_value_norm,end =" | ")
 
         # Feature 2
         feature2_value = random.uniform(32.5,42.5)
         print(feature2_name, ": ", feature2_value,end =" | ")
         feature2_value_norm = truncate((feature2_value - train_df_mean[feature2_name])/train_df_std[feature2_name],2)
-        print(feature2_name, "_norm: ", feature2_value_norm,end =" | ")
+        # print(feature2_name, "_norm: ", feature2_value_norm,end =" | ")
 
         # Feature 3
         feature3_value = random.uniform(3.0,35682.0)
         print(feature3_name, ": ", feature3_value,end =" | ")
         feature3_value_norm = truncate((feature3_value - train_df_mean[feature3_name])/train_df_std[feature3_name],2)
-        print(feature3_name, "_norm: ", feature3_value_norm,end =" | ")
+        # print(feature3_name, "_norm: ", feature3_value_norm,end =" | ")
 
         # Feature 4
         feature4_value = random.uniform(0.5,15.0)
         print(feature4_name, ": ", feature4_value, end =" | ")
         feature4_value_norm = truncate((feature4_value - train_df_mean[feature4_name])/train_df_std[feature4_name],2)
-        print(feature4_name, "_norm: ", feature4_value_norm, end =" =>  ")
+        # print(feature4_name, "_norm: ", feature4_value_norm, end =" =>  ")
 
         prediction = model.predict({feature1_name: np.array([feature1_value_norm]), 
                                 feature2_name: np.array([feature2_value_norm]), 
