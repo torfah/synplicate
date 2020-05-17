531ea0357d44871850e62da1e6f486af59b2c1ae
Learn and Verify <lv@Learns-MacBook-Pro.local>
2020-05-16 19:11:49 -0700

    new encoder Gamma + T

--- synthesizer/max_sharp_sat/encoder.py
+++ synthesizer/max_sharp_sat/encoder.py
@@ -68,137 +68,109 @@ def phi_Gamma(sat_file,samples):
     sat_file.write("\n")
     sat_file.write("phi_Gamma :=  phi_Gamma_val & phi_Gamma_det;\n")
    
+# assign feature nodes to unique features
+def phi_T_feature_det(sat_file,num_of_feature_nodes,feature_partition):
+    sat_file.write("phi_T_feature_det :=\n")
+
+    sat_file.write("T")
+    for node in range(num_of_feature_nodes):
+        sat_file.write(" &\n")
+        sat_file.write("(F")
+        for feature in feature_partition.keys():
+            sat_file.write(" | ")
+            sat_file.write("(")
+            sat_file.write(f"lam_{node:d}_{feature}")
+            for featurep in feature_partition.keys():
+                if featurep!=feature:
+                    sat_file.write(" & ")
+                    sat_file.write(f"!lam_{node:d}_{featurep}")
+            sat_file.write(")")
+        sat_file.write(")")
+    sat_file.write(";\n")
+
+# transition relation of template
+def phi_T_transitions(sat_file,num_of_feature_nodes,features_partition,label_partition):
+    sat_file.write("phi_T_transitions := \n")
+
+    sat_file.write("T")
+    for node in range(num_of_feature_nodes):
+        for feature, feature_buckets in features_partition.items():
+            sat_file.write(" & \n")
+            sat_file.write("(")
+            sat_file.write(f"lam_{node:d}_{feature} =>\n")
+            sat_file.write("          (T")
+            for feature_bucket in range(feature_buckets):
+                sat_file.write(" &\n")
+                sat_file.write("          (F")
+                for nodep in range(node+1,num_of_feature_nodes):
+                    sat_file.write(" | ")
+                    sat_file.write("(")
+                    sat_file.write(f"tau_{node:d}_{feature_bucket:d}_{nodep}")
+                    for nodepp in range(node+1,num_of_feature_nodes):
+                        if nodepp != nodep:
+                            sat_file.write(" & ")
+                            sat_file.write(f"!tau_{node:d}_{feature_bucket:d}_{nodepp}")
+                    for label, label_buckets in label_partition.items():
+                        for label_bucket in range(label_buckets):
+                            sat_file.write(" & ")
+                            sat_file.write(f"!tau_{node:d}_{feature_bucket:d}_{label}_{label_bucket}")
+                    sat_file.write(")")
+                for label, label_buckets in label_partition.items():
+                    for label_bucket in range(label_buckets):
+                        sat_file.write(" | ")
+                        sat_file.write("(")
+                        sat_file.write(f"tau_{node:d}_{feature_bucket:d}_{label}_{label_bucket:d}")
+                        for label_bucketp in range(label_buckets):
+                            if label_bucketp != label_bucket:
+                                sat_file.write(" & ")
+                                sat_file.write(f"!tau_{node:d}_{feature_bucket:d}_{label}_{label_bucketp}")
+                        for nodep in range(node+1,num_of_feature_nodes):
+                            sat_file.write(" & ")
+                            sat_file.write(f"!tau_{node:d}_{feature_bucket:d}_{nodep}")
+                        sat_file.write(")")
+                sat_file.write(")")
+            sat_file.write(")")
+            sat_file.write(")")
+    sat_file.write(";\n")
+
+# only buckets of feature in node are allowed
+def phi_T_transitions_consistency(sat_file,num_of_feature_nodes,feature_partition,label_partition):
+    sat_file.write("phi_T_transitions_consistency :=\n")
+
+    # compute maximum number of buckets
+    max_bucket = 0
+    for buckets in feature_partition.values():
+        if buckets>max_bucket:
+            max_bucket = buckets 
+
+    sat_file.write("T")
+    for node in range(num_of_feature_nodes):
+        for feature, feature_buckets in feature_partition.items():
+            sat_file.write(" &\n")
+            sat_file.write("(")
+            sat_file.write(f"lam_{node:d}_{feature} => ")
+            sat_file.write("(T")
+            for feature_bucket in range(feature_buckets,max_bucket):
+                for nodep in range(node+1,num_of_feature_nodes):
+                    sat_file.write(" & ")
+                    sat_file.write(f"!tau_{node}_{feature_bucket:d}_{nodep:d}")
+                for label,label_buckets in label_partition.items():
+                    for label_bucket in range(label_buckets):
+                        sat_file.write(" & ")
+                        sat_file.write(f"!tau_{node}_{feature_bucket:d}_{label}_{label_bucket:d}")
+            sat_file.write(")")
+            sat_file.write(")")
+    sat_file.write(";\n")
 
 def phi_T(sat_file,num_of_feature_nodes,feature_partition,label_partition):
-    num_of_features = len(feature_partition)
-    sat_file.write("phi_T :=\n")
-    # assign feature nodes to unique features
-    sat_file.write("(\n")
-    for i in range(num_of_feature_nodes):
-        sat_file.write("(")
-        for h, f in enumerate(list(feature_partition.keys())):
-            sat_file.write("(")
-            sat_file.write(f"lam_{i:d}_{f}")
-            for ff in list(feature_partition.keys()):
-                if ff!=f:
-                    sat_file.write(" & ")
-                    sat_file.write(f"!lam_{i:d}_{ff}")
-            sat_file.write(")")
-            if h<num_of_features-1:
-                sat_file.write(" | ")
-        sat_file.write(")")
-        if i<num_of_feature_nodes-1:
-            sat_file.write(" & ")
-        sat_file.write("\n")
-    sat_file.write(")")
 
-    # uniquness per feature and determinism of transition relation 
-    # uniquness feature nodes
-    sat_file.write(" \n& \n")
-    
-    if (num_of_feature_nodes>1):
-        sat_file.write("(\n")
-        for i in range(num_of_feature_nodes):
-            for fid, f in enumerate(feature_partition.keys()):
-                sat_file.write("(")
-                sat_file.write(f"lam_{i:d}_{f} => T ")
-                for j in range(i+1,num_of_feature_nodes):
-                    ffid = 0
-                    for  ff, ffbu in feature_partition.items():
-                        if j!=i and ff!=f:
-                            for s in range(ffbu):
-                                sat_file.write(f"  & !tau_{i:d}_{ff}_{s:d}_{j:d}")
-                                # if not ((ffid==num_of_features-1 or (ffid==num_of_features-2 and fid==num_of_features-1)) and (j==num_of_feature_nodes-1 or (j==num_of_feature_nodes-2 and i==num_of_feature_nodes-1)) and s==ffbu-1):
-                                #     sat_file.write(" & ")
-                        ffid += 1
-                sat_file.write(")")
-                if not(i==num_of_feature_nodes-1 and fid==num_of_features-1):
-                    sat_file.write(" & ")
-                sat_file.write("\n")
-        sat_file.write(" ) \n& \n")
-    # uniquness label nodes
-    sat_file.write("(")
-    for i in range(num_of_feature_nodes):
-        for fid, f in enumerate(feature_partition.keys()):
-            sat_file.write("(")
-            sat_file.write(f"lam_{i:d}_{f} => ")
-            lid = 0
-            for l, lbu in label_partition.items():
-                for h in range(lbu):
-                    ffid = 0
-                    for  ff, ffbu in feature_partition.items():
-                        if ff!=f:
-                            for s in range(ffbu):
-                                sat_file.write(f"   !tau_{i:d}_{ff}_{s:d}_{l}_{h}")
-                                if not ((ffid==num_of_features-1 or (ffid==num_of_features-2 and fid==num_of_features-1)) and (lid==len(label_partition)-1 and h==lbu-1) and s==ffbu-1):
-                                    sat_file.write(" & ")
-                        ffid += 1
-                lid += 1
-            sat_file.write(")")
-            if not(i==num_of_feature_nodes-1 and fid==num_of_features-1):
-                sat_file.write(" & ")
-            sat_file.write("\n")
-    sat_file.write(")\n")
-    # determinism
-    num_of_label_nodes = 0
-    lable_node_id = {}
-    for l, lbu in label_partition.items():
-        for j in range(lbu):
-            lable_node_id[num_of_label_nodes+j]= (l,j)
-        num_of_label_nodes += lbu
-
-    
-    # if num_of_feature_nodes>1:
-    sat_file.write("& \n")
-    sat_file.write("(\n")
-    for i in range(num_of_feature_nodes):
-        sat_file.write("(\n")
-        fid = 0 
-        for f,fbu in feature_partition.items():
-            for b in range(fbu):
-                sat_file.write("(")
-                for j in range(i+1,num_of_feature_nodes+num_of_label_nodes):
-                    if j < num_of_feature_nodes:
-                        if j!=i:
-                            sat_file.write("(")
-                            sat_file.write(f"tau_{i:d}_{f}_{b:d}_{j:d} & ")
-                            for jj in range(i+1,num_of_feature_nodes+num_of_label_nodes):
-                                if j!=jj and jj!=i:
-                                    if jj<num_of_feature_nodes:
-                                        sat_file.write(f"!tau_{i:d}_{f}_{b:d}_{jj:d}")
-                                    else:
-                                        sat_file.write(f"!tau_{i:d}_{f}_{b:d}_{lable_node_id[jj-num_of_feature_nodes][0]}_{lable_node_id[jj-num_of_feature_nodes][1]}")
-                                    if not(jj==num_of_feature_nodes+num_of_label_nodes-1 or (jj==num_of_feature_nodes+num_of_label_nodes-2 and j==num_of_feature_nodes+num_of_label_nodes-1)):
-                                        sat_file.write(" & ")
-                            sat_file.write(")")
-                            sat_file.write("  | ")
-                    else:
-                        sat_file.write("(")
-                        sat_file.write(f"tau_{i:d}_{f}_{b:d}_{lable_node_id[j-num_of_feature_nodes][0]}_{lable_node_id[j-num_of_feature_nodes][1]} ")
-                        for jj in range(i+1,num_of_feature_nodes+num_of_label_nodes):
-                            if jj<num_of_feature_nodes:
-                                if jj!=j and jj!=i:
-                                    sat_file.write(" & ")
-                                    sat_file.write(f"!tau_{i:d}_{f}_{b:d}_{jj:d} ")                                            
-                            else:
-                                if (lable_node_id[j-num_of_feature_nodes][0]==lable_node_id[jj-num_of_feature_nodes][0] and jj!=j):
-                                    sat_file.write(" & ")     
-                                    sat_file.write(f"!tau_{i:d}_{f}_{b:d}_{lable_node_id[jj-num_of_feature_nodes][0]}_{lable_node_id[jj-num_of_feature_nodes][1]}")                                               
-                        sat_file.write(")")
-                        if not (j==num_of_feature_nodes+num_of_label_nodes-1):
-                                sat_file.write("  | ")
-                sat_file.write(")")
-                if not (fid==num_of_features-1 and b==fbu-1):
-                    if b==fbu-1:
-                        sat_file.write("\n | \n")
-                    else:
-                        sat_file.write("&\n")
-            fid += 1 
-        sat_file.write("\n)")   
-        if not (i==num_of_feature_nodes-1):
-            sat_file.write("\n&\n")
-    sat_file.write("\n)")
-    sat_file.write(";\n")
+    phi_T_feature_det(sat_file,num_of_feature_nodes,feature_partition)
+    sat_file.write("\n")
+    phi_T_transitions(sat_file,num_of_feature_nodes,feature_partition,label_partition)
+    sat_file.write("\n")
+    phi_T_transitions_consistency(sat_file,num_of_feature_nodes,feature_partition,label_partition)
+    sat_file.write("\n")
+    sat_file.write("phi_T := phi_T_feature_det & phi_T_transitions & phi_T_transitions_consistency;\n")
 
 def phi_sim(sat_file,num_of_feature_nodes,feature_partition,label_partition,samples,feature_defs):
 
@@ -391,6 +363,7 @@ def extract_ind_variables(dimacs,name_list):
     return vars
 
 
+
 def encode(output_path,samples,num_of_feature_nodes,feature_partition,label_partition,feature_defs):
 
     # create max#sat file
@@ -401,16 +374,17 @@ def encode(output_path,samples,num_of_feature_nodes,feature_partition,label_part
 
     # phi_Gamma 
     phi_Gamma(sat_file,samples)
+    sat_file.write("\n")
 
     # phi_T
-    # phi_T(sat_file,num_of_feature_nodes,feature_partition,label_partition)
+    phi_T(sat_file,num_of_feature_nodes,feature_partition,label_partition)
 
     # phi_sim 
     # compute mapping between (feature,bucket) -> sample
     # phi_sim(sat_file,num_of_feature_nodes,feature_partition,label_partition,samples,feature_defs)
 
     sat_file.write("\n")
-    sat_file.write("FORMULA := phi_Gamma & phi_T & phi_sim;\n")
+    sat_file.write("FORMULA := phi_Gamma & phi_T;\n")
     sat_file.write("ASSIGN FORMULA;")
     sat_file.close()
 
@@ -423,32 +397,32 @@ def encode(output_path,samples,num_of_feature_nodes,feature_partition,label_part
 
     print("Translating to maxdimacs...")
     dimacs_file_path = output_path+"encoding.dimacs"
-    # maximization_vars = extract_max_variables(dimacs_tempfile_path)
-    # name_list = []
-    # for k,v in samples.items():
-    #     for i in range(len(k)):
-    #         name_list.append(k[i][0])
-    #     for j in range(len(v)):
-    #         name_list.append(v[j][0])
-    #     break
-    # counting_vars = extract_ind_variables(dimacs_tempfile_path,name_list)
-    # dimacs_file = open(dimacs_file_path,"w")
-    # dimacs_file.write("c max")
-    # for s in maximization_vars:
-    #     dimacs_file.write(f" {s} ")
-    # dimacs_file.write("0\n")    
+    maximization_vars = extract_max_variables(dimacs_tempfile_path)
+    name_list = []
+    for k,v in samples.items():
+        for i in range(len(k)):
+            name_list.append(k[i][0])
+        for j in range(len(v)):
+            name_list.append(v[j][0])
+        break
+    counting_vars = extract_ind_variables(dimacs_tempfile_path,name_list)
+    dimacs_file = open(dimacs_file_path,"w")
+    dimacs_file.write("c max")
+    for s in maximization_vars:
+        dimacs_file.write(f" {s} ")
+    dimacs_file.write("0\n")    
 
-    # dimacs_file.write("c ind")
-    # for s in counting_vars:
-    #     dimacs_file.write(f" {s} ")
-    # dimacs_file.write("0\n") 
+    dimacs_file.write("c ind")
+    for s in counting_vars:
+        dimacs_file.write(f" {s} ")
+    dimacs_file.write("0\n") 
 
-    # dimacs_temp_file = open(dimacs_tempfile_path,"r")
-    # temp_string = dimacs_temp_file.read()
-    # dimacs_file.write(temp_string)
-    # dimacs_temp_file.close()
-    # dimacs_file.close()
+    dimacs_temp_file = open(dimacs_tempfile_path,"r")
+    temp_string = dimacs_temp_file.read()
+    dimacs_file.write(temp_string)
+    dimacs_temp_file.close()
+    dimacs_file.close()
 
-    # os.system(f"rm {output_path}encoding_temp.dimacs")
+    os.system(f"rm {output_path}encoding_temp.dimacs")
 
     return dimacs_file_path
