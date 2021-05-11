6f2c95fdcaee6f21eedae8a8fdc11282cbcaf8a9
LVX <lvx@LVXs-MacBook-Pro.local>
2021-05-10 21:08:50 -0700

    new encoding with thresholds:erm

--- methods/erm/erm_procedure.py
+++ methods/erm/erm_procedure.py
@@ -36,19 +36,22 @@ def execute(benchmark_path, synthesizer, delta, epsilon,name,samples={}):
     # Execute synthesizer
     
     synthesis_time = 0
+    upper_bound = 50
+    lower_bound = 0
+    precision = 7
 
     # dump input samples
     logger.dump_samples(samples,benchmark_path,f"erm_syn_samples")
 
     start = timeit.default_timer()
-    program_path, dot_path, count = synthesizer.synthesize(benchmark_path,samples,f"{name}_erm")
+    program_path, dot_path, count = synthesizer.synthesize(benchmark_path,samples,lower_bound,upper_bound,precision,f"{name}_erm")
     print(f"Synthesized program: {program_path}\nVisualization: {dot_path}")
     stop = timeit.default_timer()
     synthesis_time = stop-start
     print(f"Synthesis time:{synthesis_time}")
 
     if count>0:
-        print(f"Misclassification rate:{1-count/num_of_samples}")
+        print(f"Accuracy:{count/num_of_samples}")
 
     return samples, program_path
         
